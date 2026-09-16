92d9f41b0e8f5279e0b6203ae07cdfc8eb7a5b63
Ксения <ksusavolkova322@gmail.com>
2026-09-16 21:36:38 +0300

    Begin18.py

---
+++ Begin18.py
@@ -0,0 +1,7 @@
+A = float(input("Введите координату"))
+B = float(input("Введите координату"))
+C = float(input("Введите координату"))
+AC = C - A
+BC = B - C
+pr = AC * BC
+print(f"Длина отрезка AC {AC}", f"Длина отрезка BC = {BC}", f"Произведение AC и BC = {pr}")
