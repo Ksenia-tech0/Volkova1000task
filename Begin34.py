e3859f0492a7216d95e5c970a41db82104825d12
Ксения <ksusavolkova322@gmail.com>
2026-09-16 21:42:40 +0300

    Begin34.py

---
+++ Begin34.py
@@ -0,0 +1,8 @@
+X = float(input("Введите число"))
+A = float(input("Введите число"))
+Y = float(input("Введите число"))
+B = float(input("Введите число"))
+g = A / X
+i = B / Y
+o = g / i
+print(f"стоит {g}", f"стоит {i}", f"стоит {o}")
