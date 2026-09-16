3f8c9f51e9aa5129635c732e9a689e0d9bc3460b
Ксения <ksusavolkova322@gmail.com>
2026-09-16 21:36:15 +0300

    Begin17.py

---
+++ Begin17.py
@@ -0,0 +1,7 @@
+A = float(input("Введите координату"))
+B = float(input("Введите координату"))
+C = float(input("Введите координату"))
+AC = C - A
+BC = C - B
+sum = AC + BC
+print(f"Длина отрезка AC {AC}", f"Длина отрезка BC = {BC}", f"Сумма AC и BC = {sum}")
