903e13a56789737086803d653ca0e630d4fa5001
Ксения <ksusavolkova322@gmail.com>
2026-09-16 21:43:11 +0300

    Begin35.py

---
+++ Begin35.py
@@ -0,0 +1,8 @@
+V = float(input("Введите число"))
+U = float(input("Введите число"))
+T1 = float(input("Введите число"))
+T2 = float(input("Введите число"))
+S1 = V * T1
+S2 = (V - U) * T2
+S = S1 + S2
+print(f"Общий путь {S}")
